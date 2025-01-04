1c4b5a63ce7df913c118bc1e4f8791927950ae31
developerpauloricardo <developerpauloricardo@gmail.com>
2025-01-04 09:36:54 -0300

    Add: react hook form, toastify, yup e atualização do login

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -1,7 +1,63 @@
 import { Header } from "../../components/header";
 import styles from "./styles.module.css";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { ToastContainer, toast, Bounce } from "react-toastify";
+
+type Props = {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  senha: string;
+  confirmeSenha: string;
+};
+
+const schema = yup
+  .object({
+    nome: yup.string().required("O nome é obrigatório"),
+    sobrenome: yup.string().required("O sobrenome é obrigatório"),
+    email: yup
+      .string()
+      .email("Digite um e-mail válido")
+      .required("O e-mail é obrigatório"),
+    senha: yup
+      .string()
+      .min(6, "A senha precisa ter no minímo 6 digitos")
+      .required("A senha é obrigatório"),
+    confirmeSenha: yup
+      .string()
+      .required("Confirmar a senha é obrigatório")
+      .oneOf([yup.ref("senha")], "As senhas precisam ser iguais"),
+  })
+  .required();
+
 function Cadastro() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Props>({ resolver: yupResolver(schema) });
+
+  const handleFormulario: SubmitHandler<Props> = (data) => {
+    toast.success("Conta criada com sucesso!", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+    reset();
+    console.log(data);
+  };
+
   return (
     <div className={styles.container}>
       <Header
@@ -9,40 +65,52 @@ function Cadastro() {
         subtitulo="Bem-vindo, novamente ! Entre com seu e-mail e senha para acessar sua conta da Bankly"
       />
 
-      <form action="#">
+      <form action="#" onSubmit={handleSubmit(handleFormulario)}>
         <div className={styles.itens}>
           <div className={styles.item}>
             <label htmlFor="nome">Nome</label>
-            <input type="text" placeholder="Digite o seu nome" id="nome" />
-            <span></span>
+            <input
+              type="text"
+              placeholder="Digite o seu nome"
+              {...register("nome", { required: true })}
+            />
+            <span> {errors.nome?.message}</span>
           </div>
           <div className={styles.item}>
             <label htmlFor="email">Sobrenome</label>
             <input
               type="text"
-              placeholder="Digite o seu e-mail"
-              id="sobrenome"
+              placeholder="Digite o seu sobrenome"
+              {...register("sobrenome", { required: true })}
             />
-            <span></span>
+            <span> {errors.sobrenome?.message}</span>
           </div>
           <div className={styles.item}>
             <label htmlFor="email">E-mail</label>
-            <input type="email" placeholder="Digite o seu e-mail" id="email" />
-            <span></span>
+            <input
+              type="email"
+              placeholder="Digite o seu e-mail"
+              {...register("email", { required: true })}
+            />
+            <span> {errors.email?.message}</span>
           </div>
           <div className={styles.item}>
             <label htmlFor="senha">Senha</label>
-            <input type="password" placeholder="Digite sua senha" id="senha" />
-            <span></span>
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              {...register("senha", { required: true })}
+            />
+            <span> {errors.senha?.message}</span>
           </div>
           <div className={styles.item}>
             <label htmlFor="confirme-senha">Repita sua Senha</label>
             <input
               type="password"
               placeholder="Digite sua senha"
-              id="confirme-senha"
+              {...register("confirmeSenha", { required: true })}
             />
-            <span></span>
+            <span> {errors.confirmeSenha?.message}</span>
           </div>
 
           <div className={styles.itensTwo}>
@@ -63,6 +131,7 @@ function Cadastro() {
           </div>
         </div>
       </form>
+      <ToastContainer />
     </div>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,54 @@
 import { Header } from "../../components/header";
 import styles from "./styles.module.css";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { ToastContainer, toast, Bounce } from "react-toastify";
+
+type Props = {
+  email: string;
+  senha: string;
+};
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Digite um e-mail válido")
+      .required("O e-mail é obrigatório"),
+    senha: yup
+      .string()
+      .min(6, "A senha precisa ter no minímo 6 digitos")
+      .required("A senha é obrigatório"),
+  })
+  .required();
+
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Props>({ resolver: yupResolver(schema) });
+
+  const handleFormulario: SubmitHandler<Props> = (data) => {
+    toast.success("Redirecionando", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+    reset();
+    console.log(data);
+  };
+
   return (
     <div className={styles.container}>
       <Header
@@ -9,17 +56,25 @@ function Login() {
         subtitulo="Bem-vindo, novamente ! Entre com seu e-mail e senha para acessar sua conta da Bankly"
       />
 
-      <form action="#">
+      <form action="#" onSubmit={handleSubmit(handleFormulario)}>
         <div className={styles.itens}>
           <div className={styles.item}>
             <label htmlFor="email">E-mail</label>
-            <input type="email" placeholder="Digite o seu e-mail" id="email" />
-            <span></span>
+            <input
+              type="email"
+              placeholder="Digite o seu e-mail"
+              {...(register("email"), { requires: true })}
+            />
+            <span> {errors.email?.message}</span>
           </div>
           <div className={styles.item}>
             <label htmlFor="senha">Senha</label>
-            <input type="password" placeholder="Digite sua senha" id="senha" />
-            <span></span>
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              {...register("senha", { required: true })}
+            />
+            <span> {errors.senha?.message}</span>
           </div>
 
           <div className={styles.itensTwo}>
@@ -43,6 +98,7 @@ function Login() {
           </div>
         </div>
       </form>
+      <ToastContainer />
     </div>
   );
 }
